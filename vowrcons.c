a8f21342899ce118bc9275f1c661d43dddcce258
aiswariyarajiyalakshmi <aishwariya.ravichandran@gmail.com>
2016-08-13 13:56:54 +0530

    Update vowrcons.c

--- vowrcons.c
+++ vowrcons.c
@@ -2,9 +2,14 @@
 int main()
 {
 char a;
+if((a>='a'&&a<='z')||(a>='A'&&a<='Z'))
+{
 if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
 printf("vowel");
 else
 printf("consonant");
+}
+else
+printf("Not an alphabet");
 return 0;
 }
